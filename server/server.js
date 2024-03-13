2b73f912a77473cb5bad5903e189157d672ed815
Ramon Pessoa <ramonpessoad@hotmail.com>
2024-03-13 01:13:49 -0300

    Changes files

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 9001;
 
 app.use(express.json());
 
