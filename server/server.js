ebb389171a6136bcb028e4c41f32cbfac074d3e1
Ramon Pessoa <ramonpessoad@hotmail.com>
2024-03-13 01:40:18 -0300

    Change pathjsondata

--- server/server.js
+++ server/server.js
@@ -2,86 +2,49 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 9001;
+const port = process.env.PORT || 3001;
 
-app.use(express.json());
-
-const dbPath = path.join(__dirname, 'db', 'db.json');
+const jsonData = require('./db/db.json');
 
 app.get('/ingredientes', (req, res) => {
-  fs.readFile(dbPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Erro ao ler o arquivo db.json');
-    }
-    const dbJson = JSON.parse(data);
-    res.json(dbJson);
-  });
+  res.json(jsonData);
 });
 
 app.get('/burgers', (req, res) => {
-  fs.readFile(dbPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Erro ao ler o arquivo db.json');
-    }
-    const dbJson = JSON.parse(data);
-    res.json(dbJson.burgers);
-  });
+  res.json(jsonData);
 });
 
 app.get('/status', (req, res) => {
-  fs.readFile(dbPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Erro ao ler o arquivo db.json');
-    }
-    const dbJson = JSON.parse(data);
-    res.json(dbJson.status);
-  });
+  res.json(jsonData);
 });
 
 app.post('/burgers', (req, res) => {
-  fs.readFile(dbPath, 'utf8', (err, data) => {
+  const newBurger = req.body;
+  jsonData.burgers.push(newBurger);
+  fs.writeFile('./db/db.json', JSON.stringify(dbJson, null, 2), (err) => {
     if (err) {
       console.error(err);
-      return res.status(500).send('Erro ao ler o arquivo db.json');
+      return res.status(500).send('Erro ao salvar o arquivo db.json');
     }
-    const dbJson = JSON.parse(data);
-    const newBurger = req.body;
-    dbJson.burgers.push(newBurger);
-    fs.writeFile(dbPath, JSON.stringify(dbJson, null, 2), (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send('Erro ao salvar o arquivo db.json');
-      }
-      res.status(201).json(newBurger);
-    });
+    res.status(201).json(newBurger);
   });
 });
 
 app.delete('/burgers/:id', (req, res) => {
-  fs.readFile(dbPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Erro ao ler o arquivo db.json');
-    }
-    const dbJson = JSON.parse(data);
-    const id = req.params.id;
-    const index = dbJson.burgers.findIndex(burger => burger.id === id);
-    if (index !== -1) {
-      dbJson.burgers.splice(index, 1);
-      fs.writeFile(dbPath, JSON.stringify(dbJson, null, 2), (err) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).send('Erro ao salvar o arquivo db.json');
-        }
-        res.status(200).json({ id: id });
-      });
-    } else {
-      res.status(404).send('Burger não encontrado');
-    }
-  });
+  const id = req.params.id;
+  const index = dbJson.burgers.findIndex(burger => burger.id === id);
+  if (index !== -1) {
+    jsonData.burgers.splice(index, 1);
+    fs.writeFile('./db/db.json', JSON.stringify(dbJson, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Erro ao salvar o arquivo db.json');
+      }
+      res.status(200).json({ id: id });
+    });
+  } else {
+    res.status(404).send('Burger não encontrado');
+  }
 });
 
 app.listen(port, () => {
